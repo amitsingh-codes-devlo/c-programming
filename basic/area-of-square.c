321f8a5e9df709c02e4c059bd1fae8556f529e18
Amit Singh <xinghamit22@gmail.com>
2026-08-15 23:48:11 +0530

    Add program to calculate area of a square

---
+++ basic/area-of-square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+// Area of square 
+int main(){
+   int side;
+
+   printf("Enter the side of square");
+   scanf("%d",&side);
+
+   printf("the area of square : %d ",side*side);
+}
